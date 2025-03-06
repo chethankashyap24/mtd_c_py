ffa69cf3d2862ca0c6744dd373db64fa33d81fff
chethankashyap24 <chethanvenugopal@gmail.com>
2025-03-06 14:39:21 +0530

    p2.c added

--- day1/p2.c
+++ day1/p2.c
@@ -3,7 +3,7 @@ int main()
 {
     int num1=0;
     long int num2=0;
-    lomg long int num3=0;
+    long long int num3=0;
     double d=0.0;
     float f=0.0f;
     printf("%d %d %d %d %d\n", sizeof(num1), sizeof(num2), sizeof(num3), sizeof(d), sizeof(f));
